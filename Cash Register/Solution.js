ebc3761fbb393d3c9632118135938f5dfcc94a81
parresh77 <parresh77@gmail.com>
2020-11-29 03:29:12 +0530

    Improve code formatting

--- Cash Register/Solution.js
+++ Cash Register/Solution.js
@@ -2,45 +2,49 @@ var global = [];
 
 let add12 = (str, amt) =>
 {
-	if(amt != 0)
+	if (amt != 0)
+	{
 		global.push([str, parseFloat(amt)])
+	}
 }
 
 let sufficient = (change, cid) =>
 {
 	var chi = change;
 	let cidind = cid[0][1] + cid[1][1] + cid[2][1] + cid[3][1] + cid[4][1] + cid[5][1] + cid[6][1] + cid[7][1] + cid[8][1];
-	if(cidind< cid)
+	if (cidind < cid)
+	{
 		return "less";
+	}
 	else if (cidind == change)
 	{
-		return "equal"
+		return "equal";
 	}
 	else
 	{
-		let d100 = cid[8][1]/100;
-		let c100 = parseInt(change/100);
-		if(d100 > c100)
+		let d100 = cid[8][1] / 100;
+		let c100 = parseInt(change / 100);
+		if (d100 > c100)
 		{
-			cid[8][1] = cid[8][1] - c100*100;
-			change = change - c100*100;
-			add12('ONE HUNDRED', c100*100)
+			cid[8][1] = cid[8][1] - c100 * 100;
+			change = change - c100 * 100;
+			add12('ONE HUNDRED', c100 * 100)
 		}
 		else
 		{
 			cid[8][1] = 0;
 			change = change - d100 * 100;
-			add12('ONE HUNDRED', d100*100)
+			add12('ONE HUNDRED', d100 * 100)
 		}
 
-		let d20 = cid[7][1]/20;
-		let c20 = parseInt(change/20);
+		let d20 = cid[7][1] / 20;
+		let c20 = parseInt(change / 20);
 
-		if(d20 > c20)
+		if (d20 > c20)
 		{
-			cid[7][1] = cid[7][1] - c20*20;
-			change = change - c20*20;
-			add12('TWENTY', c20*20)
+			cid[7][1] = cid[7][1] - c20 * 20;
+			change = change - c20 * 20;
+			add12('TWENTY', c20 * 20)
 		}
 		else
 		{
@@ -49,39 +53,39 @@ let sufficient = (change, cid) =>
 			add12('TWENTY', d20 * 20)
 		}
 
-		let d10 = cid[6][1]/10;
-		let c10 = parseInt(change/10);
-		if(d10 > c10)
+		let d10 = cid[6][1] / 10;
+		let c10 = parseInt(change / 10);
+		if (d10 > c10)
 		{
-			cid[6][1] = cid[6][1] - c10*10;
-			change = change - c10*10;
-			add12('TEN', c10*10);
+			cid[6][1] = cid[6][1] - c10 * 10;
+			change = change - c10 * 10;
+			add12('TEN', c10 * 10);
 		}
 		else
 		{
 			cid[6][1] = 0;
 			change = change - d10 * 10;
-			add12('TEN', d10*10);
+			add12('TEN', d10 * 10);
 		}
 
-		let d5 = cid[5][1]/5;
-		let c5 = parseInt(change/5);
-		if(d5 > c5)
+		let d5 = cid[5][1] / 5;
+		let c5 = parseInt(change / 5);
+		if (d5 > c5)
 		{
-			cid[5][1] = cid[5][1] - c5*5;
-			change = change - c5*5;
-			add12('FIVE', c5*5);
+			cid[5][1] = cid[5][1] - c5 * 5;
+			change = change - c5 * 5;
+			add12('FIVE', c5 * 5);
 		}
 		else
 		{
 			cid[5][1] = 0;
 			change = change - d5 * 5;
-			add12('FIVE', d5*5);
+			add12('FIVE', d5 * 5);
 		}
 
 		let d1 = cid[4][1];
 		let c1 = parseInt(change);
-		if(d1 > c1)
+		if (d1 > c1)
 		{
 			cid[4][1] = cid[4][1] - c1;
 			change = change - c1;
@@ -94,62 +98,62 @@ let sufficient = (change, cid) =>
 			add12('ONE', d1);
 		}
 
-		let d025 = cid[3][1]*4;
-		let c025 = parseInt(change*4);
-		if(d025 > c025)
+		let d025 = cid[3][1] * 4;
+		let c025 = parseInt(change * 4);
+		if (d025 > c025)
 		{
-			cid[3][1] = cid[3][1] - c025*4;
-			change = change - c025/4;
-			add12('QUARTER', c025/4);
+			cid[3][1] = cid[3][1] - c025 * 4;
+			change = change - c025 / 4;
+			add12('QUARTER', c025 / 4);
 		}
 		else
 		{
 			cid[3][1] = 0;
 			change = change - d025 / 4;
-			add12('QUARTER', d025/4);
+			add12('QUARTER', d025 / 4);
 		}
 
-		let d01 = cid[2][1]*10;
-		let c01 = parseInt(change*10);
+		let d01 = cid[2][1] * 10;
+		let c01 = parseInt(change * 10);
 
-		if(d01 > c01)
+		if (d01 > c01)
 		{
-			cid[2][1] = cid[2][1] - c01*10;
-			change = change - c01/10;
-			add12('DIME', c01/10);
+			cid[2][1] = cid[2][1] - c01 * 10;
+			change = change - c01 / 10;
+			add12('DIME', c01 / 10);
 		}
 		else
 		{
 			cid[2][1] = 0;
-			change = change - d01/10;
-			add12('DIME', d01/10);
+			change = change - d01 / 10;
+			add12('DIME', d01 / 10);
 		}
 
-		let d005 = cid[1][1]*20;
-		let c005 = parseInt(change*20);
+		let d005 = cid[1][1] * 20;
+		let c005 = parseInt(change * 20);
 
-		if(d005 > c005)
+		if (d005 > c005)
 		{
-			cid[1][1] = cid[1][1] - c005*20;
-			change = change - c005/20;
-			add12('NICKEL', c005/20);
+			cid[1][1] = cid[1][1] - c005 * 20;
+			change = change - c005 / 20;
+			add12('NICKEL', c005 / 20);
 		}
 		else
 		{
 			cid[1][1] = 0;
-			change = change - d005/20;
-			add12('NICKEL', d005/20);
+			change = change - d005 / 20;
+			add12('NICKEL', d005 / 20);
 		}
 
 		change = change.toFixed(2);
 
-		let d001 = cid[0][1]*100;
-		let c001 = parseInt(change*100);
+		let d001 = cid[0][1] * 100;
+		let c001 = parseInt(change * 100);
 
-		if(d001 > c001)
+		if (d001 > c001)
 		{
-			cid[1][1] = cid[1][1] - c001*100;
-			var temp9 = c001/100;
+			cid[1][1] = cid[1][1] - c001 * 100;
+			var temp9 = c001 / 100;
 			temp9 = temp9.toFixed(2);
 
 			change = change - temp9;
@@ -158,35 +162,44 @@ let sufficient = (change, cid) =>
 		else
 		{
 			cid[1][1] = 0;
-			var temp99 = d001/20;
+			var temp99 = d001 / 20;
 			temp99 = temp99.toFixed(2);
+
 			change = change - temp99;
 			add12('PENNY', temp99);
 		}
 
 		let final = cid[0][1] + cid[1][1] + cid[2][1] + cid[3][1] + cid[4][1] + cid[5][1] + cid[6][1] + cid[7][1] + cid[8][1];
 
-		if(change > 0)
-		return 'nonexact';
+		if (change > 0)
+		{
+			return 'nonexact';
+		}
 		else
 		{
 			return 'more';
 		}
-
 	}
 }
 
-function checkCashRegister(price, cash, cid) {
+function checkCashRegister(price, cash, cid)
+{
 	global = [];
 	let status1 = sufficient(cash - price, cid);
 	var change1;
-	if(status1 == "less" || status1 == 'nonexact')
+
+	if (status1 == "less" || status1 == 'nonexact')
+	{
 		change1 = {status: 'INSUFFICIENT_FUNDS', change: []};
-	else if(status1 == "equal")
+	}
+	else if (status1 == "equal")
+	{
 		change1 = {status: "CLOSED", change: cid}
+	}
 	else
 	{
 		change1 = {status:"OPEN",change:global}
 	}
+
 	return change1;
 }
--- Telephone Number Validator/Solution.js
+++ Telephone Number Validator/Solution.js
@@ -1,11 +1,13 @@
 function telephoneCheck(str) {
 	if (str.split("").reverse()[0] == ")" || str.split("")[0] == "-")
+	{
 		return false;
+	}
 
 	var bracleft = str.split("(").length;
 	var bracright = str.split(")").length;
 
-	if( bracleft != bracright)
+	if ( bracleft != bracright)
 	{
 		return false;
 	}
@@ -15,16 +17,12 @@ function telephoneCheck(str) {
 	str = str.split("-").join("");
 	str = str.split(" ").join("");
 
-	if(str.length == 11)
+	if (str.length == 11)
 	{
-		if(str.split("")[0] == "1")
-			return true;
-		return false;
-	}
-	else if(str.length == 10)
-	{
-		return true;
+		return str.split("")[0] == "1"
 	}
 	else
-		return false;
+	{
+		return str.length == 10;
+	}
 }
